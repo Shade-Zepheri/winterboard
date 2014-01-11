8542ae03a64bf3b05b1e94ddb023615d8c9bb4f4
Jay Freeman (saurik) <saurik@saurik.com>
2014-01-11 04:23:37 -0800

    For some reason iOS 7 Calendar keeps sizing "23".

--- Library.mm
+++ Library.mm
@@ -910,7 +910,11 @@ MSInstanceMessageHook1(UIImage *, SBCalendarApplicationIcon, generateIconImage, 
         "text-shadow: rgba(0, 0, 0, 0.2) -1px -1px 2px;"
     , @"CalendarIconDayStyle"};
 
-    WBStringDrawingState sizeState = {&dayState, 7, nil, nil};
+    WBStringDrawingState wtfState = {&dayState, 1, nil, nil};
+
+    WBStringDrawingState sizeState = {
+        kCFCoreFoundationVersionNumber > 800 ? &wtfState : &dayState
+    , 7, nil, nil};
 
     WBStringDrawingState dateState = {&sizeState, 2, @""
     , @"CalendarIconDateStyle"};
