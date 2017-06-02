c3e73922974f4be80d26297661ea13e67f551388
Shade-Zepheri <ziroalpha@gmail.com>
2017-06-01 22:15:04 -0500

    Removed wb$colorForKey

--- NSDictionary.h
+++ NSDictionary.h
@@ -1,4 +1,3 @@
 @interface NSDictionary (WinterBoard)
-- (UIColor*)wb$colorForKey:(NSString *)key;
 - (BOOL)wb$boolForKey:(NSString *)key;
 @end
--- NSDictionary.m
+++ NSDictionary.m
@@ -1,18 +1,14 @@
 #import "NSDictionary.h"
 
 @implementation NSDictionary (WinterBoard)
-- (UIColor*) wb$colorForKey:(NSString *)key {
-    NSString *value = [self objectForKey:key];
-    if (value == nil)
-        return nil;
-    /* XXX: incorrect */
-    return nil;
-}
 
-- (BOOL) wb$boolForKey:(NSString *)key {
-    if (NSString *value = [self objectForKey:key])
-        return [value boolValue];
-    return false;
+- (BOOL)wb$boolForKey:(NSString *)key {
+    NSString *value = [self objectForKey:key]
+    if (value) {
+      return [value boolValue];
+    }
+
+    return NO;
 }
 
 @end
